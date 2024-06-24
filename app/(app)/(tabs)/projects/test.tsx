cb4aa93d7d513490e245cc3382176fedc1084299
edmondbozan <edmond.bozan@gmail.com>
2024-06-24 16:18:02 -0400

    uploading images

--- app/(app)/(tabs)/projects/test.tsx
+++ app/(app)/(tabs)/projects/test.tsx
@@ -10,6 +10,7 @@ import { ProjectDetails } from '@/interfaces/IProject';
 import { normalize } from 'react-native-elements';
 import VideoButton from '@/components/VideoButton';
 import { ResizeMode, Video } from 'expo-av';
+import { BASE_URL } from '@/constants/Endpoints';
 
 interface Item {
     id: number;
@@ -21,9 +22,9 @@ interface Item {
 const App: React.FC = () => {
     const [items, setItems] = useState<ProjectDetails[]>([]);
     const [description, setDescription] = useState<string>('');
-    const [beforeImage, setBeforeImage] = useState<string | null>(null);
-    const [videoUri, setVideo] = useState<string | null>(null);
-    const [afterImage, setAfterImage] = useState<string | null>(null);
+    const [beforeImage, setBeforeImage] = useState<ImagePicker.ImagePickerAsset | null>(null);
+    const [videoUri, setVideo] = useState<ImagePicker.ImagePickerAsset | null>(null);
+    const [afterImage, setAfterImage] = useState<ImagePicker.ImagePickerAsset | null>(null);
     const [image, setImage] = useState<string | null>(null);
     const { data } = useLocalSearchParams();
     const projectData = data ? JSON.parse(data as string) : null;
@@ -32,58 +33,74 @@ const App: React.FC = () => {
 
     // const projectData = data ? JSON.parse(data as string) : null;
 
-    const pickImage = async (
-        setImage: React.Dispatch<React.SetStateAction<string | null>>,
-        mediaType: ImagePicker.MediaTypeOptions,
-    ) => {
+    const pickImage = async (setImage: React.Dispatch<React.SetStateAction<ImagePicker.ImagePickerAsset | null>>, mediaType: ImagePicker.MediaTypeOptions) => {
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: mediaType,
-            allowsEditing: true,
-            aspect: [1, 1],
-            quality: 1,
+          mediaTypes: mediaType,
+          allowsEditing: true,
+          aspect: [1, 1],
+          quality: 1,
         });
-
+    
         if (!result.canceled) {
-            setImage(result.assets[0].uri);
+          setImage(result.assets[0]);
         }
-    };
+      };
 
-  const Cancel =() =>{
-         
-    router.replace({   
-        pathname: '/projects/project',
-       params: { data : JSON.stringify(projectData) }});
-  }
+    const Cancel = () => {
 
-    const addItem = () => {
-               
+        router.replace({
+            pathname: '/projects/project',
+            params: { data: JSON.stringify(projectData) }
+        });
+    }
+
+    const addItem = async () => {
+
         const newItem: ProjectDetails = {
-        projectDetailId: 0,
-        description: description,
-            afterImage: afterImage,
-            beforeImage:beforeImage,
-            video:videoUri};
+            projectDetailId: 0,
+            description: description,
+            afterImage: afterImage?.uri || null,
+            beforeImage: beforeImage?.uri || null,
+            video: videoUri?.uri || null
+        }
 
-            const updatedProjectData = [...projectData];
-            // Add the new item to the details array of the first project
-            updatedProjectData[0].details = [...updatedProjectData[0].details, newItem];
-            setItems(updatedProjectData[0].details);
-            
-            let formData = new FormData();
+        const updatedProjectData = [...projectData];
+        updatedProjectData[0].details = [...updatedProjectData[0].details, newItem];
+        setItems(updatedProjectData[0].details);
 
-        Alert.alert(JSON.stringify(afterImage));
-             if(afterImage != null){
-            Alert.alert("adding");
+        let formData = new FormData();
+
+        if (afterImage != null) {
             formData.append('files', {
-                uri: afterImage,
-                name: 'photo_after.jpg',
-                type: 'image/jpeg',
-              } as any)
+                uri: afterImage.uri,
+                name: afterImage.fileName || 'photo_after.jpg',
+                type: afterImage.type || 'image/jpeg',            } as any)
+        }
+        formData.append('description', description);
+
+        // for (let pair of formData.entries()) {
+        //     console.log(`${pair[0]}: ${pair[1]}`);
+        // }
+                try {
+            const response = await fetch(BASE_URL + '/api/Upload/upload', {
+                method: 'POST',
+                body: formData,
+                // headers: {
+                //     'Content-Type': 'multipart/form-data',
+                // },
+            });
+            const responseText = await response.text(); // Read response as text
+
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${responseText}`);
             }
-           
-
-
-
+    
+            // Parse the response text as JSON
+            const responseData = JSON.parse(responseText);
+            console.log(responseData);         
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
 
         // Reset form
         setDescription('');
@@ -94,38 +111,39 @@ const App: React.FC = () => {
 
 
 
-        router.replace({   
+        router.replace({
             pathname: '/projects/project',
-           params: { data : JSON.stringify(projectData) }});
-    
-    //    Alert.alert(JSON.stringify(projectData[0].details[2]));
+            params: { data: JSON.stringify(projectData) }
+        });
+
+        //    Alert.alert(JSON.stringify(projectData[0].details[2]));
     };
 
     return (
         <SafeAreaView style={styles.container}>
-         <Text onPress={()=>{
-            router.replace({   
-                 pathname: '/projects/project',
-                params: { data : projectData }});
-                }}>
-             <FontAwesome name="arrow-left"/> Projects
-             </Text>              
+            <Text onPress={() => {
+                router.replace({
+                    pathname: '/projects/project',
+                    params: { data: projectData }
+                });
+            }}>
+                <FontAwesome name="arrow-left" /> Projects
+            </Text>
 
             <Provider>
-                <View style={styles.form}>  
+                <View style={styles.form}>
                     <View style={styles.photoBox}>
-                        <Text style={{marginBottom:10}}>Show off your work with before and after pics </Text>
+                        <Text style={{ marginBottom: 10 }}>Show off your work with before and after pics </Text>
                         <View style={styles.photoRow}>
-                            <CameraButton label="After" onPress={() => pickImage(setAfterImage, ImagePicker.MediaTypeOptions.Images)} imageUri={afterImage} />
+                        <CameraButton label="After" onPress={() => pickImage(setAfterImage, ImagePicker.MediaTypeOptions.Images)} imageUri={afterImage?.uri || null} />
                             <View style={styles.space}></View>
-                            <CameraButton label="Before" onPress={() => pickImage(setBeforeImage, ImagePicker.MediaTypeOptions.Images)} imageUri={beforeImage} />
-                        </View> 
-                                               
-                    </View>                    
+                            <CameraButton label="Before" onPress={() => pickImage(setBeforeImage, ImagePicker.MediaTypeOptions.Images)} imageUri={beforeImage?.uri || null} />                        
+                        </View>
+                    </View>
                     <View style={styles.photoBox}>
-                        <Text style={{marginBottom:10}}>Upload a Video </Text>
+                        <Text style={{ marginBottom: 10 }}>Upload a Video </Text>
                         <View style={styles.photoRow}>
-                            <VideoButton label="Video" onPress={() => pickImage(setVideo, ImagePicker.MediaTypeOptions.Videos)} videoUri={videoUri}  />
+                            <VideoButton label="Video" onPress={() => pickImage(setVideo, ImagePicker.MediaTypeOptions.Videos)} videoUri={videoUri?.uri || null} />
                         </View>
                     </View>
                     <View>
@@ -134,25 +152,25 @@ const App: React.FC = () => {
                             value={description}
                             onChangeText={setDescription}
                             style={styles.input}
-//                            multiline={true}
+                            //                            multiline={true}
                             maxLength={500}
                         />
                     </View>
 
                     <View style={styles.buttons}>
-          <View style={[styles.buttonContainer]}>
-          <TouchableOpacity  onPress={addItem} >
-              <Text style={styles.button}>  Add</Text>
-            </TouchableOpacity>
+                        <View style={[styles.buttonContainer]}>
+                            <TouchableOpacity onPress={addItem} >
+                                <Text style={styles.button}>  Add</Text>
+                            </TouchableOpacity>
 
-            </View>
-            <View style={styles.space} /> 
-            <View style={styles.buttonContainer}>
-            <TouchableOpacity  onPress={Cancel} >
-              <Text style={styles.button}>Cancel</Text>
-            </TouchableOpacity>
-            </View>
-          </View>
+                        </View>
+                        <View style={styles.space} />
+                        <View style={styles.buttonContainer}>
+                            <TouchableOpacity onPress={Cancel} >
+                                <Text style={styles.button}>Cancel</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
 
 
                 </View>
@@ -169,33 +187,33 @@ const styles = StyleSheet.create({
     },
     buttons: {
         flexDirection: 'row',
-         justifyContent: 'space-evenly',
+        justifyContent: 'space-evenly',
         padding: 30,
-      },
-    
+    },
+
 
     buttonContainer: {
         backgroundColor: '#e4eaf7',
-        borderColor:'#B87333',
-        borderWidth:1.5,
+        borderColor: '#B87333',
+        borderWidth: 1.5,
         borderRadius: 15, // Rounded edges
-//        height:20
-        padding: 20,    
-        justifyContent:'center',
-        paddingVertical:8,
+        //        height:20
+        padding: 20,
+        justifyContent: 'center',
+        paddingVertical: 8,
         shadowColor: '#000', // Shadow for a subtle depth effect
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.1,
         shadowRadius: 0,
         elevation: 5, // For Android shadow
-        color:'#000',
-        width:100,
-      },
-      button:{
-        fontSize:18,
+        color: '#000',
+        width: 100,
+    },
+    button: {
+        fontSize: 18,
         fontWeight: 'black',
-        color:'#000',
-      },
+        color: '#000',
+    },
 
 
     photoBox: {
@@ -203,7 +221,7 @@ const styles = StyleSheet.create({
         borderWidth: .5,
         padding: 10,
         borderRadius: 10,
-        marginBottom:10
+        marginBottom: 10
     },
     photoRow: {
         flexDirection: 'row',
@@ -217,16 +235,16 @@ const styles = StyleSheet.create({
         // height: 250,
         height: normalize(200),
         borderColor: '#B87333',
-        backgroundColor:"#fff",
+        backgroundColor: "#fff",
         borderRadius: 10,
         shadowColor: '#000',
         color: "#000",
         elevation: 5,
         borderWidth: normalize(1),
-//        marginBottom: normalize(10),
-  //      paddingHorizontal: normalize(10),
+        //        marginBottom: normalize(10),
+        //      paddingHorizontal: normalize(10),
         width: '100%',
-        
+
     },
     card: {
         marginBottom: 10,
